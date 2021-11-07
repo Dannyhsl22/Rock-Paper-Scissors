1842a284be40a3d524032ffb266e4b2e41147f53
Dannyhsl22 <dannyhawasli@gmail.com>
2021-11-07 10:39:22 +0000

    add game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,50 @@ const scissors_div = document.getElementById("scissors");
 const lizard_div = document.getElementById("lizard");
 const spock_div = document.getElementById("spock");
 
-function getComputererChoice() {
+function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     const randomNumber = (Math.floor(Math.random() * 5));
     return choices[randomNumber];
 }
-console.log(getComputererChoice());
+
 
 function game(userChoice) {
-
+    const computerChoice = getComputerChoice();
+switch (userChoice + computerChoice) {
+    case "rockscissors":
+    case "rocklizard":
+    case "scissorspaper":
+    case "scissorslizard":
+    case "paperrock":
+    case "paperspock":
+    case "lizardspock":
+    case "lizardpaper":
+    case "spockscissors":
+    case "spockrock":
+    console.log("USER WINS!.");
+    break;
+    case "scissorsrock":
+    case "lizardrock":
+    case "paperscissors":
+    case "lizardscissors":
+    case "rockpaper":
+    case "spockpaper":
+    case "spocklizard":
+    case "paperlizard":
+    case "scissorsspock":
+    case "rockspock":
+    console.log("USER LOSES!.")
+    break;
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+    case "lizardlizard":
+    case "spockspock":
+    console.log("IT IS A TIE!.")
+    break;
 }
+}
+
 
 function main(){
     rock_div.addEventListener('click', function() {
